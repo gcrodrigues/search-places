a7c05eae39bf9b90df6b228e44957d3d79979a79
Gustavo <gcrodrigues413@gmail.com>
2020-07-19 00:47:01 -0300

    Desenvolvimento da tela Home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,166 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useEffect, useState } from "react";
+import * as Location from "expo-location";
+import {
+  TextInput,
+  View,
+  Text,
+  Alert,
+  ActivityIndicator,
+  Keyboard,
+} from "react-native";
+import { RectButton } from "react-native-gesture-handler";
+import { Feather } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+import MapView, { Marker } from "react-native-maps";
+import api, { API_KEY } from "../../service/api";
+import FlashMessage from "../../components/FlashMessage";
 
 import styles from "./styles";
 
+interface Place {
+  id: string;
+  place_id: string;
+  geometry: {
+    location: {
+      lat: number;
+      lng: number;
+    };
+  };
+  name: string;
+  opening_hours: {
+    open_now: boolean;
+  };
+  types: string[];
+  vicinity: string;
+}
+
 const Home: React.FC = () => {
-  return <View />;
+  const [userLocation, setUserLocation] = useState<[number, number]>([0, 0]);
+  const [queryValue, setQueryValue] = useState("");
+  const [places, setPlaces] = useState<Place[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [status, setStatus] = useState(true);
+  const [flashMessage, setFlashMessage] = useState(false);
+
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    loadPosition();
+  }, []);
+
+  async function loadPosition() {
+    const { status } = await Location.requestPermissionsAsync();
+
+    if (status !== "granted") {
+      Alert.alert(
+        "Ooops...",
+        "Precisamos de sua permissão para obter a sua localização!"
+      );
+      setStatus(false);
+      return;
+    }
+    setStatus(true);
+    const location = await Location.getCurrentPositionAsync();
+    const { latitude, longitude } = location.coords;
+    setUserLocation([latitude, longitude]);
+  }
+
+  function handlePlacesData() {
+    setLoading(true);
+    api
+      .get(`nearbysearch/json`, {
+        params: {
+          key: API_KEY,
+          location: `${userLocation[0]},${userLocation[1]}`,
+          rankby: "distance",
+          name: queryValue,
+        },
+      })
+      .then((res) => {
+        setPlaces(res.data.results);
+        Keyboard.dismiss();
+        setLoading(false);
+        setFlashMessage(true);
+      });
+    setTimeout(() => {
+      setFlashMessage(false);
+    }, 10000);
+  }
+
+  function handleNavigateToDetail(id: string) {
+    navigation.navigate("Detail", { place_id: id });
+  }
+
+  return !status ? (
+    <View style={styles.noPermissionContainer}>
+      <Feather name="alert-triangle" size={50} color="black" />
+      <Text style={styles.noPermissionText}>
+        Permita o acesso a sua localização para ter acesso ao app.
+      </Text>
+      <RectButton style={styles.noPermissionButton} onPress={loadPosition}>
+        <Text style={styles.noPermissionButtonText}>
+          Permitir acesso a localização
+        </Text>
+      </RectButton>
+    </View>
+  ) : (
+    <View style={styles.container}>
+      {flashMessage && <FlashMessage />}
+      <View style={styles.contentWrapper}>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            value={queryValue}
+            onChangeText={(text) => setQueryValue(text)}
+            placeholder="Pesquise por estabelecimentos"
+            returnKeyType={"search"}
+            onSubmitEditing={handlePlacesData}
+          />
+          <RectButton style={styles.button} onPress={handlePlacesData}>
+            {loading ? (
+              <ActivityIndicator />
+            ) : (
+              <Feather name="search" size={20} color="#000" />
+            )}
+          </RectButton>
+        </View>
+
+        {userLocation[0] !== 0 && (
+          <MapView
+            initialRegion={{
+              latitude: userLocation[0],
+              longitude: userLocation[1],
+              latitudeDelta: 0.01,
+              longitudeDelta: 0.01,
+            }}
+            style={styles.mapStyle}
+            showsUserLocation={true}
+            showsMyLocationButton={false}
+            showsCompass={false}
+            rotateEnabled={false}
+          >
+            {places.map(
+              (place, index) =>
+                index <= 9 && (
+                  <Marker
+                    key={place.id}
+                    onPress={() => handleNavigateToDetail(place.place_id)}
+                    coordinate={{
+                      latitude: place.geometry.location.lat,
+                      longitude: place.geometry.location.lng,
+                    }}
+                  >
+                    <View style={styles.markerView}>
+                      <Text style={styles.markerText}>{index + 1}</Text>
+                    </View>
+                  </Marker>
+                )
+            )}
+          </MapView>
+        )}
+      </View>
+    </View>
+  );
 };
 
 export default Home;
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,2 +1,91 @@
 import { StyleSheet } from "react-native";
-export default StyleSheet.create({});
+import Constants from "expo-constants";
+
+export default StyleSheet.create({
+  noPermissionContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingHorizontal: 20,
+  },
+  noPermissionText: {
+    fontSize: 20,
+    textAlign: "center",
+    marginVertical: 10,
+  },
+  noPermissionButton: {
+    alignItems: "stretch",
+    backgroundColor: "#3c6",
+    paddingVertical: 15,
+    paddingHorizontal: 25,
+    borderRadius: 5,
+    marginTop: 10,
+  },
+  noPermissionButtonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+
+  container: {
+    flex: 1,
+    alignItems: "center",
+    position: "relative",
+  },
+  contentWrapper: {
+    width: "100%",
+    height: "100%",
+    position: "relative",
+    alignItems: "center",
+  },
+  inputContainer: {
+    width: "95%",
+    flexDirection: "row",
+    backgroundColor: "#fff",
+    borderRadius: 25,
+    borderWidth: 1,
+    borderColor: "#d9d9d9",
+    borderStyle: "solid",
+    marginTop: Constants.statusBarHeight + 22,
+    position: "absolute",
+    zIndex: 2,
+  },
+  input: {
+    width: "85%",
+    backgroundColor: "#fff",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 25,
+    fontSize: 18,
+    color: "#333",
+  },
+  button: {
+    width: "15%",
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 50,
+  },
+  mapStyle: {
+    width: "100%",
+    height: "100%",
+  },
+  markerView: {
+    width: 25,
+    height: 25,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 25 / 2,
+    backgroundColor: "red",
+    borderColor: "#505050",
+    borderStyle: "solid",
+    borderWidth: 1,
+  },
+  markerText: {
+    color: "white",
+    fontWeight: "bold",
+    // textAlign: "center",
+    fontSize: 14,
+    // marginBottom: 10,
+  },
+});
