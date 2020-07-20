1ad090ce7c3dfca835d65d64089f6f8a354b84e3
Gustavo <gcrodrigues413@gmail.com>
2020-07-19 22:08:16 -0300

    Desenvolvimento da tela Detail

--- src/screens/Detail/index.tsx
+++ src/screens/Detail/index.tsx
@@ -1,10 +1,141 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  ScrollView,
+  Text,
+  View,
+  Linking,
+  ActivityIndicator,
+} from "react-native";
+import { Feather } from "@expo/vector-icons";
+import { useRoute, useNavigation } from "@react-navigation/native";
+import api, { API_KEY } from "../../service/api";
+import { Rating } from "react-native-ratings";
+import { RectButton } from "react-native-gesture-handler";
 
 import styles from "./styles";
 
+interface Params {
+  place_id: string;
+  open_now: boolean;
+}
+
+interface PlaceDetail {
+  formatted_address: string;
+  formatted_phone_number: string;
+  name: string;
+  rating: number;
+  website: string;
+}
+
 const Detail: React.FC = () => {
-  return <View />;
+  const route = useRoute();
+  const navigation = useNavigation();
+  const routeParams = route.params as Params;
+  const [data, setData] = useState<PlaceDetail>({} as PlaceDetail);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    async function fetchPlaceDetails() {
+      setLoading(true);
+      try {
+        const response = await api.get(`details/json`, {
+          params: {
+            key: API_KEY,
+            place_id: routeParams.place_id,
+            fields:
+              "formatted_address,formatted_phone_number,name,opening_hours,rating,website",
+          },
+        });
+        setData(response.data.result);
+        setLoading(false);
+      } catch (error) {
+        console.log("Erro! Não foi possível obter os dados da requisição");
+        setLoading(false);
+      }
+    }
+    fetchPlaceDetails();
+  }, []);
+
+  function handleNavigateBack() {
+    navigation.goBack();
+  }
+
+  function handlePhoneCall() {
+    Linking.openURL(`tel:${data.formatted_phone_number}`);
+  }
+
+  return (
+    <ScrollView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <RectButton onPress={handleNavigateBack}>
+          <Feather name="arrow-left" size={26} color="black" />
+        </RectButton>
+        <RectButton onPress={handlePhoneCall}>
+          <Feather name="phone-call" size={26} color="#3c6" />
+        </RectButton>
+      </View>
+      <View>
+        <View style={styles.header}>
+          <Text style={styles.placeName}>{data.name}</Text>
+          <View style={styles.rating}>
+            <Text>{data.rating} </Text>
+            <Rating
+              readonly={true}
+              imageSize={18}
+              tintColor="#f0f0f5"
+              startingValue={data.rating}
+              ratingBackgroundColor="#ddd"
+              type="custom"
+            />
+          </View>
+        </View>
+        <View style={styles.placeDetails}>
+          <View style={styles.placeDetailsCell}>
+            <Feather name="map" size={22} color="#27f" />
+            {loading ? (
+              <ActivityIndicator style={{ marginLeft: 20 }} />
+            ) : (
+              <Text style={styles.placeAddress}>{data.formatted_address}</Text>
+            )}
+          </View>
+          <View style={styles.placeDetailsCell}>
+            <Feather name="phone" size={22} color="#27f" />
+            {loading ? (
+              <ActivityIndicator style={{ marginLeft: 20 }} />
+            ) : (
+              <Text style={styles.placePhone}>
+                {data.formatted_phone_number}
+              </Text>
+            )}
+          </View>
+          <View style={styles.placeDetailsCell}>
+            <Feather name="clock" size={22} color="#27f" />
+            {loading ? (
+              <ActivityIndicator style={{ marginLeft: 20 }} />
+            ) : (
+              <Text style={styles.placeOpen}>
+                {routeParams.open_now ? "Aberto" : "Fechado"}
+              </Text>
+            )}
+          </View>
+          <View style={styles.placeDetailsCell}>
+            <Feather name="globe" size={22} color="#27f" />
+            {loading ? (
+              <ActivityIndicator style={{ marginLeft: 20 }} />
+            ) : data.website ? (
+              <Text
+                style={styles.placeWebsite}
+                onPress={() => Linking.openURL(`${data.website}`)}
+              >
+                {data.website}
+              </Text>
+            ) : (
+              <Text style={styles.noWebsite}>Estabelecimento sem site</Text>
+            )}
+          </View>
+        </View>
+      </View>
+    </ScrollView>
+  );
 };
 
 export default Detail;
--- src/screens/Detail/styles.ts
+++ src/screens/Detail/styles.ts
@@ -1,2 +1,57 @@
 import { StyleSheet } from "react-native";
-export default StyleSheet.create({});
+import Constants from "expo-constants";
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    justifyContent: "space-between",
+    flexDirection: "row",
+    paddingTop: 20 + Constants.statusBarHeight,
+  },
+  header: {
+    flex: 1,
+    paddingHorizontal: 20,
+    borderBottomColor: "#bbb",
+    borderBottomWidth: 1,
+    borderStyle: "solid",
+  },
+  placeName: {
+    fontSize: 24,
+  },
+  rating: {
+    flex: 1,
+    alignItems: "center",
+    flexDirection: "row",
+    marginBottom: 20,
+  },
+  placeDetails: {
+    flex: 1,
+  },
+  placeDetailsCell: {
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+    alignItems: "center",
+    flexDirection: "row",
+  },
+  placeAddress: {
+    marginLeft: 20,
+  },
+  placePhone: {
+    marginLeft: 20,
+  },
+  placeOpen: {
+    marginLeft: 20,
+    fontWeight: "bold",
+  },
+  placeWebsite: {
+    marginLeft: 20,
+  },
+  noWebsite: {
+    marginLeft: 20,
+    fontStyle: "italic",
+    color: "#c9c9c9",
+  },
+});
